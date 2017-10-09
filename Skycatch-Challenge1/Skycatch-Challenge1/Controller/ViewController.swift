7b676f5870d21694b41db4664ecafc9bc4450b13
Mitchell Schuster <mgschuster11@gmail.com>
2017-10-09 16:38:49 +0300

    main UI Complete

--- Skycatch-Challenge1/Skycatch-Challenge1/Controller/ViewController.swift
+++ Skycatch-Challenge1/Skycatch-Challenge1/Controller/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  Skycatch-Challenge1
 //
 //  Created by Admin on 10/9/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
